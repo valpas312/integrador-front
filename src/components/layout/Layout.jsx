ed1478c8c4acd4d05f458e6ea30b294d4eabbbfb
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-08-25 16:40:42 -0300

    boton para desplegar sidebar

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,19 +1,34 @@
 import Navbar from "../navbar/Navbar";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Button, useDisclosure } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
+
+  const { isOpen, onToggle } = useDisclosure();
+
   return (
     <>
       <Navbar />
       <Grid
         templateAreas={`"nav main"`}
         gridTemplateRows={"1fr"}
-        gridTemplateColumns={"200px 1fr"}
+        gridTemplateColumns={isOpen ? "200px 1fr" : "50px 1fr"}
         h="100vh"
       >
         <GridItem bg="#FF686B" boxShadow="2xl" area={"nav"}>
-          Nav
+          <Button
+            w="100%"
+            borderRadius={0}
+            bg="white"
+            onClick={onToggle}
+          >
+            {
+              isOpen ?
+                <ChevronLeftIcon /> :
+                <ChevronRightIcon />
+            }
+          </Button>
         </GridItem>
         <GridItem area={"main"}>
           {children}
