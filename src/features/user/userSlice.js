3bf1ccd7c6961354c52d20ae50597608b01899c6
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-08-31 11:30:10 -0300

    .

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,25 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    value: {}
+  value: {},
 };
 
 const tokenSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        setUser: (state, action) => {
-            state.value = action.payload;
-        },
-        logout: (state) => {
-            state.value = {};
-        },
-        setVerified: (state) => {
-            state.value.verified = true;
-        },
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.value = action.payload;
     },
+    logout: (state) => {
+      state.value = {};
+    },
+    setVerified: (state) => {
+      state.value.verified = true;
+    },
+  },
 });
 
-export const { setUser, logout, setVerified} = tokenSlice.actions;
+export const { setUser, logout, setVerified } = tokenSlice.actions;
 
 export default tokenSlice.reducer;
